b6cdfe46bde56da79b5a04bfb6ec20a32a497061
Matthew Grasinger <grasingerm@gmail.com>
2015-03-25 21:32:11 -0400

    visual test of particle collisions

---
+++ test/vtest_collide.jl
@@ -0,0 +1,39 @@
+println("Testing srt_col_f...");
+
+require("collision.jl");
+require("defaults.jl");
+require("lattice.jl");
+require("multiscale.jl");
+require("simulate.jl");
+
+lat = Lattice(1.0, 1.0, 10, 10, 9.0);
+msm = MultiscaleMap(0.1, lat);
+
+# initialize lattice
+for k=1:length(lat.w)
+  lat.f[5,5,k] = 10.0;
+end
+map_to_macro!(lat, msm);
+
+println("initial conditions");
+println("rho=")
+println(msm.rho);
+println("u=");
+println(msm.u[:,:,1]);
+println("v=");
+println(msm.u[:,:,2]);
+
+temp_f = zeros(lat.f);
+
+for i=1:4
+  stream!(lat, temp_f);
+  srt_col_f!(lat, msm);
+  map_to_macro!(lat, msm);
+  println("step i=", i);
+  println("rho=")
+  println(msm.rho);
+  println("u=");
+  println(msm.u[:,:,1]);
+  println("v=");
+  println(msm.u[:,:,2]);
+end
