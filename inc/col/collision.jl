f49ddb583e0c20a57866f2bd1b41930abfd3cf52
Matthew Grasinger <grasingerm@gmail.com>
2015-07-02 13:42:18 -0600

    Added `feq_f` to modular collision functions
    Add: feq_f as a parameters to collision functions

--- inc/col/collision.jl
+++ inc/col/collision.jl
@@ -1,4 +1,5 @@
 const __collision_root__ = dirname(@__FILE__);
-require(abspath(joinpath(__collision_root__, "freecol.jl")));
-require(abspath(joinpath(__collision_root__, "modcol.jl")));
-require(abspath(joinpath(__collision_root__, "stdcol.jl")));
+require(abspath(joinpath(__collision_root__, "freecol.jl"))); # free surface collisions
+require(abspath(joinpath(__collision_root__, "modcol.jl")));  # "modular" collisions
+require(abspath(joinpath(__collision_root__, "pmodcol.jl"))); # parallel mod collisions
+require(abspath(joinpath(__collision_root__, "stdcol.jl")));  # "standard" collisions
