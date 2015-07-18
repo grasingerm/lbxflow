4af7b67c0e996c72761f3135b92c3a5c1e3f8bdb
Matthew Grasinger <grasingerm@gmail.com>
2015-07-17 21:53:19 -0400

    Changed all source code in this project to the MPLv2 license

--- inc/numerics.jl
+++ inc/numerics.jl
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #! Compute the double dot product of two matrices
 function ddot(A::Array{Float64,2}, B::Array{Float64,2})
   sum = 0;
