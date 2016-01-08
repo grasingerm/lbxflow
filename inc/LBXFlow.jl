44174d44e45e33ae561d2a32ccdcf83058efcf49
Matthew Grasinger <grasingerm@gmail.com>
2016-01-08 08:09:43 -0500

    Cleaned up include paths and cli
    Rem: removed 'debug' mode from cli
    Ref: simplified include paths

--- inc/LBXFlow.jl
+++ inc/LBXFlow.jl
@@ -12,10 +12,4 @@ include(joinpath("sim","simulate.jl"));
 using PyCall;
 @pyimport yaml;
 
-export simulate!, sim_step!;
-export MultiscaleMap, map_to_macro!, strain_rate_tensor;
-export north_bounce_back!, south_bounce_back!, east_bounce_back!,           ...
-       west_bounce_back!, north_half_bounce_back!, south_half_bounce_back!, ...
-       east_half_bounce_back!, 
-
 end
--- inc/api.jl
+++ inc/api.jl
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 push!(LOAD_PATH, ".");
-using LBXFlow
+using LBXFlow;
 
 function parse_and_run(infile::AbstractString, args::Dict)
 
