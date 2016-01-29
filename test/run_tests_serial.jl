966c3a44b3363e6b21caee4f19580bb95e381537
Matthew Grasinger <grasingerm@gmail.com>
2016-01-29 10:59:03 -0500

    modified run_tests_serial.jl script so that it runs all tests and displays results

--- test/run_tests_serial.jl
+++ test/run_tests_serial.jl
@@ -1,7 +1,3 @@
-include("test_multiscale.jl");
-include("test_constitutive.jl");
-include("test_tracking.jl");
-
 if isfile("lbxflow.jl")
   const main = abspath("lbxflow.jl");
 elseif isfile(joinpath("..", "lbxflow.jl"))
@@ -10,10 +6,27 @@ else
   error("Cannot find lbxflow.jl. Please run script from test directory");
 end
 
-const __run_tests_root__ = dirname(@__FILE__); 
+const __run_tests_root__  = dirname(@__FILE__); 
 
-const test_input_files = filter(s -> endswith(s, ".yaml"), readdir(__run_tests_root__));
-for test_input_file in test_input_files
+const test_input_files    = filter(s -> endswith(s, ".yaml"), readdir(__run_tests_root__));
+test_results              = fill(true, length(test_input_files));
+for (i, test_input_file) in enumerate(test_input_files)
   full_path_to_input_file = joinpath(__run_tests_root__, test_input_file);
-  run(`julia $main -vf $full_path_to_input_file`);
+  try
+    run(`julia $main -vf $full_path_to_input_file`);
+  catch e
+    test_results[i] = false; 
+  end
+end
+
+println("Test results");
+println("============");
+for (test, result) in zip(test_input_files, results)
+  @printf("%40s: ", test);
+  if result
+    print_with_color(:green, "Passed.");
+  else
+    print_with_color(:red, "FAILED.");
+  end
+  print("\n");
 end
