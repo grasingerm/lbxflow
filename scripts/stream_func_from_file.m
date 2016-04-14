8aa40e90b9d40e8c5a038707c48071d6decf54b8
Matthew Grasinger <grasingerm@gmail.com>
2016-04-14 12:39:43 -0400

    fixed stream_func_from_file matlab function

--- scripts/stream_func_from_file.m
+++ scripts/stream_func_from_file.m
@@ -1,6 +1,6 @@
-function phi, psi = stream_func_from_file(u_file, v_file)
-  u = transpose(csvread(u_file));
-  v = transpose(csvread(v_file));
+function [phi, psi] = stream_func_from_file(u_file, v_file)
+  u = csvread(u_file);
+  v = csvread(v_file);
 
-  phi, psi = flowfun(u, v);
+  [phi, psi] = flowfun(u, v);
 end
