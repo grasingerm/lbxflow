4ce587c5693bd09b6d297c1fe52f3fcf10e84385
Matthew Grasinger <grasingerm@gmail.com>
2016-06-03 02:51:06 -0400

    started writing code for two-phase flow

--- inc/col/equilibrium.jl
+++ inc/col/equilibrium.jl
@@ -93,3 +93,35 @@ function feq_incomp_max_entropy(lat::LatticeD2Q9, rho::AbstractFloat,
 
   return lat.w[k] * rho * prod;
 end
+
+#! Equilibrium distribution for immisible two-phase flow
+#!
+#! \param lat D2Q9 lattice
+#! \param rho Macroscopic density at lattice site
+#! \param u Macroscopic flow at lattice site
+#! \param k Lattice velocity vector index
+#! \param α Free paramter related to surface tension
+#! \return Equilibrium particle distribution
+function feq_incomp_mphase_immis(lat::LatticeD2Q9, rho::AbstractFloat,
+                                 u::AbstractArray{Float64, 1}, k::Int,
+                                 α::Real)
+  const ckdotu = dot(lat.c[:,k], u);
+
+  if k == 9
+    return rho * (α - 2/3 * dot(u, u));
+  elseif k > 0 && k < 5
+    return (rho * ((1-α)/5 + lat.w[k]*
+                   (3*ckdotu + 9/2*ckdotu*ckdotu - 3/2 * dot(u, u))));
+  elseif k > 4 && k < 9
+    return (rho * ((1-α)/20 + lat.w[k]*
+                   (3*ckdotu + 9/2*ckdotu*ckdotu - 3/2 * dot(u, u))));
+  else
+    error("k must be between 1 and 9 for D2Q9 lattice");
+    return 0.0;
+  end
+end
+
+#! Binds α to a two-phase immisible equilibrium function
+function init_feq_mphase_immis(α::Real)
+  return (@anon (lat, rho, u, k) -> feq_incomp_mphase_immis(lat, rho, u, k, α));
+end
